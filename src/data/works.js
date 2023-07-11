7a7d30796c2a8d1c337d9428e5349e994b6bfa94
camilo180601 <camilo06180401@gmail.com>
2023-07-11 00:10:25 -0500

    update portfolio

--- src/data/works.js
+++ src/data/works.js
@@ -3,7 +3,7 @@ export const works = [
         'id': 'translator',
         'name': 'GPT-3.5 API translator',
         'url': 'https://github.com/camilo180601/translate-react-api-gpt-3.5',
-        'technologies': 'ReactJS, TypeScript, API GPT-3.5, HTML, CSS',
+        'technologies': 'ReactJS, TypeScript, API GPT-3.5, HTML and CSS',
         'category': 'Web development',
         'description': 'The project is a translation application built using React and the OpenAI GPT-3.5 API. It allows users to input text in one language and receive an accurate translation in another language. The application features an intuitive interface, language selection options, real-time translations, and adaptability across different devices. It leverages the power of the GPT-3.5 model to deliver high-quality translations, and its scalability enables it to handle a high volume of translation requests. In summary, it is a modern and efficient tool for text translation.'
     },
@@ -11,7 +11,7 @@ export const works = [
         'id': 'socialmedia',
         'name': 'Social Media',
         'url': 'https://github.com/camilo180601/redsocial',
-        'technologies': 'Laravel, MySQL, jQuery, JavaScript, HTML, CSS',
+        'technologies': 'Laravel, MySQL, jQuery, JavaScript, HTML, CSS and Bootstrap',
         'category': 'Web development',
         'description': 'The project is a photo-sharing social media application built with Laravel. Users can register, upload photos, interact with others content, and manage their profiles. Key features include user authentication, photo upload and management, social interaction, privacy controls, explore and discover functionality, notifications and messaging, search and filtering, and mobile responsiveness. It provides a platform for users to share and discover captivating visual content within a community-driven environment.'
     },
@@ -19,7 +19,7 @@ export const works = [
         'id': 'ecommerce',
         'name': 'E-commerce',
         'url': 'https://github.com/camilo180601/dakiti',
-        'technologies': 'PHP, MySQL, jQuery, JavaScript, HTML, Bootstrap',
+        'technologies': 'PHP, MySQL, jQuery, JavaScript, HTML, CSS and Bootstrap',
         'category': 'Web development',
         'description': 'The project is a PHP-native e-commerce website with PayPal integration for secure payment processing. It features a product catalog, shopping cart, order management, product reviews, search and filtering, and a responsive design. Users can browse products, add items to the cart, and complete transactions using PayPal. However, the project does not include user login or authentication functionalities. Overall, it provides a user-friendly and convenient platform for online shopping.'
     },
@@ -27,7 +27,7 @@ export const works = [
         'id': 'appmovies',
         'name': 'Movie App',
         'url': 'https://github.com/camilo180601/app-movies',
-        'technologies': 'ReactJS, JavaScript, HTML, CSS',
+        'technologies': 'ReactJS, JavaScript, HTML and CSS',
         'category': 'Web development',
         'description': 'The project is a React-based app for movie covers. Users can add and filter movie covers to create a personalized collection. It offers features like filtering, sorting, and a user-friendly interface. The app allows users to easily manage and explore their movie cover collection.'
     },
@@ -35,8 +35,16 @@ export const works = [
         'id': 'blog',
         'name': 'Blog Web',
         'url': 'https://github.com/camilo180601/blog',
-        'technologies': 'PHP, HTML, CSS, MySQL',
+        'technologies': 'PHP, HTML, CSS and MySQL',
         'category': 'Web development',
         'description': 'The PHP-native blog project offers users the ability to manage their posts with features like editing and deleting. It incorporates a login system for user authentication and ensures a responsive design for a seamless browsing experience. With categories, related topics, and user-friendly functionalities, it provides a comprehensive platform for content creation and management.'
+    },
+    {
+        'id': 'crudnode',
+        'name': 'Crud NodeJS',
+        'url': 'https://github.com/camilo180601/CRUD-NodeJS',
+        'technologies': 'NodeJS, Express, MySQL, HTML, CSS and Boostrap',
+        'category': 'Web development',
+        'description': 'This project involves building a CRUD (Create, Read, Update, Delete) application using Node.js and Express. It allows users to perform basic database operations such as creating, reading, updating, and deleting records.'
     }
 ];
