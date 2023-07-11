c22acaa8b6da7e10ef9ec8674d1e9dac7dc2f19f
camilo180601 <camilo06180401@gmail.com>
2023-07-11 02:07:52 -0500

    update form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ const Contact = () => {
         <div className='page'>
             <h1 className='heading'>Contact</h1>
             <form className='contact' action="mailto:camilo06180401@gmail.com">
-                <input type='text' placeholder='Name' />
-                <input type='text' placeholder='Last name' />
-                <input type='email' placeholder='Email' />
-                <textarea placeholder='Reason for contact' />
+                <input type='text' placeholder='Name' autocomplete="on" />
+                <input type='text' placeholder='Last name' autocomplete="on" />
+                <input type='email' placeholder='Email' autocomplete="on" />
+                <textarea placeholder='Reason for contact' autocomplete="on" />
                 <input type='submit' value='Send' />
             </form>
         </div>
